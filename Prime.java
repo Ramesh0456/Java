98e065cc9d6cfd256af662388c10f83e63a2b4d6
Ramesh Marupakula <32768707+Ramesh0456@users.noreply.github.com>
2018-03-27 22:12:00 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,28 @@
+package rakesh;
+
+import java.util.Scanner;
+
+public class Prime
+{
+	public static void main(String[] args)
+	{
+		System.out.println("enter any num");
+		Scanner sc=new Scanner(System.in);
+		int	n=sc.nextInt();
+		int p=0; 
+			for(int j=2;j<n;j++)
+			{
+				
+				if(n%j==0) 
+				{
+				
+					p=1;
+					System.out.println("not a prime");
+					break;					
+				}
+			}	
+			if(p==0)			
+				System.out.println("prime");
+	}	
+}
+
